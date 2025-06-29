7f895becb970648cabfdc9ea678cccefca0e99be
SprikerS <jeremyestelo17@gmail.com>
2025-06-29 15:56:47 -0500

    feat: add search field to PaginationDto for enhanced query capabilities

--- src/common/dtos/pagination.dto.ts
+++ src/common/dtos/pagination.dto.ts
@@ -1,7 +1,11 @@
 import { Type } from 'class-transformer'
-import { IsOptional, IsPositive, Min } from 'class-validator'
+import { IsOptional, IsPositive, IsString, Min } from 'class-validator'
 
 export class PaginationDto {
+  @IsOptional()
+  @IsString()
+  search?: string
+
   @IsOptional()
   @IsPositive()
   @Type(() => Number)
